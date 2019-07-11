f6b4cdb840b0841b249b9deabcbfd16805bf3a12
sichangyong <1822607099@qq.com>
2019-07-11 22:12:06 +0800

    首页

--- open_clientele/src/main/java/com/wd/health/activity/MainActivity.java
+++ open_clientele/src/main/java/com/wd/health/activity/MainActivity.java
@@ -1,9 +1,15 @@
 package com.wd.health.activity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.dingtao.common.core.WDActivity;
@@ -17,17 +23,51 @@ import com.wd.health.fragment.WardmateFragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import butterknife.BindView;
+
 @Route(path = Constant.ACTIVITY_LOGIN_MAIN)
 public class MainActivity extends WDActivity {
     @BindView(R2.id.mainfragment)
     FrameLayout mainfragment;
     @BindView(R2.id.mainradio)
     RadioGroup mainradio;
+    @BindView(R2.id.mainbt)
+    ImageView mainbt;
+    @BindView(R2.id.mainshowhide)
+    RelativeLayout mainshowhide;
+    @BindView(R2.id.bt2)
+    RadioButton bt2;
+    @BindView(R2.id.bt3)
+    RadioButton bt3;
+    @BindView(R2.id.bt4)
+    RadioButton bt4;
     private FragmentManager manager;
     private HomePagerFragement homePagerFragement;
     private WardmateFragment wardmateFragment;
     private VideoFragment videoFragment;
     private FragmentTransaction transaction;
+    private int page = 3;
+    //创建hind
+    public Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            page--;
+            handler.sendEmptyMessageDelayed(0, 1000);
+            if (bt3.isChecked()==false){
+                mainshowhide.setVisibility(View.GONE);
+                mainradio.setVisibility(View.VISIBLE);
+                page=3;
+                handler.removeMessages(0);
+            }else{
+                if (page == 0) {
+                    mainradio.setVisibility(View.GONE);//导航消失
+                    mainshowhide.setVisibility(View.VISIBLE);//小导航栏出现
+                    page=3;
+                    handler.removeMessages(0);
+                }
+            }
+        }
+    };
 
     @Override
     protected int getLayoutId() {
@@ -46,7 +86,7 @@ public class MainActivity extends WDActivity {
                     .hide(wardmateFragment)
                     .hide(videoFragment)
                     .commit();
-        }else{  // 正常时
+        } else {  // 正常时
             homePagerFragement = new HomePagerFragement();
             wardmateFragment = new WardmateFragment();
             videoFragment = new VideoFragment();
@@ -65,18 +105,49 @@ public class MainActivity extends WDActivity {
                 transaction = manager.beginTransaction();
                 if (checkedId == R.id.bt1) {
                     transaction.show(homePagerFragement).hide(wardmateFragment).hide(videoFragment);
-
+                    bt2.setVisibility(View.VISIBLE);
+                    bt4.setVisibility(View.GONE);
                 } else if (checkedId == R.id.bt2) {
-                    transaction.show(wardmateFragment).hide(homePagerFragement).hide(videoFragment);
-
+                    for (int i = 0; i < 2; i++) {
+                        if (bt2.isChecked()){
+                            bt2.setChecked(false);
+                            bt2.setVisibility(View.VISIBLE);
+                            bt4.setVisibility(View.GONE);
+                        }else{
+                            transaction.show(wardmateFragment).hide(homePagerFragement).hide(videoFragment);
+                            bt4.setVisibility(View.VISIBLE);
+                            bt2.setVisibility(View.GONE);
+                            bt4.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    Toast.makeText(MainActivity.this, "正在开发", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    }
                 } else if (checkedId == R.id.bt3) {
                     transaction.show(videoFragment).hide(homePagerFragement).hide(wardmateFragment);
-
+                    mainradio.setVisibility(View.GONE);//导航消失
+                    mainshowhide.setVisibility(View.VISIBLE);//小导航栏出现
+                    bt2.setVisibility(View.VISIBLE);
+                    bt4.setVisibility(View.GONE);
                 }
                 transaction.commit();
             }
         });
+        //点击切换底部栏
+        mainbt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainradio.setVisibility(View.VISIBLE);//导航消失
+                mainshowhide.setVisibility(View.GONE);//小导航栏出现
+                handler.sendEmptyMessageDelayed(0, 1000);
+            }
+        });
+        //点击发表病友圈
+
     }
+    //写一个方法
 
     @Override
     protected void initView() {
